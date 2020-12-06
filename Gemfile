ed400903ed46112ca667d38fcb4976e3834e00f1
Courtney Payne <cpayne624@gmail.com>
2020-12-06 14:32:47 -0500

    Genesis

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.7.0'
+
+gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
+gem 'pg', '~> 1.2', '>= 1.2.3'
+gem 'puma', '~> 4.1'
+gem 'sass-rails', '>= 6'
+gem 'turbolinks', '~> 5'
+gem 'jbuilder', '~> 2.7'
+gem 'bootsnap', '>= 1.4.2', require: false
+gem 'dokno', '~> 1.0'
+
+group :development, :test do
+  gem 'pry-byebug', '~> 3.9'
+end
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '~> 3.2'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
---
+++ config/initializers/dokno.rb
@@ -0,0 +1,14 @@
+Dokno.configure do |config|
+  config.tag_whitelist          = Dokno::Config::TAG_WHITELIST
+  config.attr_whitelist         = Dokno::Config::ATTR_WHITELIST
+
+  # To restrict Dokno data modification and include indentifying information
+  # in change log entries, provide the appropriate user values for your app below.
+  #   (String) app_user_object
+  #   (Symbol) app_user_auth_method
+  #   (Symbol) app_user_name_method
+
+  # config.app_user_object      = 'current_user'
+  # config.app_user_auth_method = :admin?
+  # config.app_user_name_method = :name
+end
